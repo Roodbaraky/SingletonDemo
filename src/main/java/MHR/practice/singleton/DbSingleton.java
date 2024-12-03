46cdaad4f72a7e3f3df0d0713fb829dcbfd69bb0
Roodbaraky <kooroshr@hotmail.com>
2024-12-03 16:35:00 +0000

    Added h2 database

--- src/main/java/MHR/practice/singleton/DbSingleton.java
+++ src/main/java/MHR/practice/singleton/DbSingleton.java
@@ -1,20 +1,33 @@
 package MHR.practice.singleton;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class DbSingleton {
 
-    //    private static DbSingleton instance = new DbSingleton();
+    private Connection conn = null;
 
     private static class LazyHolder {
         static final DbSingleton INSTANCE = new DbSingleton();
     }
 
     private DbSingleton() {
+        //make connection to DB when singleton is instantiated
+        try {
+            String jdbcURL = "jdbc:h2:mem:test";
+            conn = DriverManager.getConnection(jdbcURL);
 
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 
     public static DbSingleton getInstance() {
 
-        //    return instance;
         return LazyHolder.INSTANCE;
+    }
+    public Connection getConnection(){
+        return conn;
     }
 }
--- src/main/java/MHR/practice/singleton/DbSingletonDemo.java
+++ src/main/java/MHR/practice/singleton/DbSingletonDemo.java
@@ -1,11 +1,23 @@
 package MHR.practice.singleton;
 
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 public class DbSingletonDemo {
 
-    public static void main(String [] args){
+    public static void main(String[] args) throws SQLException {
         DbSingleton instance = DbSingleton.getInstance();
 
         System.out.println(instance);
-
+        Connection conn = instance.getConnection();
+        Statement statement = conn.createStatement();
+        statement.execute("CREATE table students (ID int PRIMARY KEY, name varchar(65))");
+        System.out.println("Create table students");
+        int rows = statement.executeUpdate("insert into students (ID, name) values (1, 'Koo')");
+        if (rows > 0) {
+            System.out.println("Inserted a new row");
+        }
+        conn.close();
     }
 }
