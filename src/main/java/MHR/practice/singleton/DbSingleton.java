a357adf6dee97a4eeb51c92e34abf9b11d3589fb
Roodbaraky <kooroshr@hotmail.com>
2024-12-03 16:24:14 +0000

    Amended to LazyLoadinh / thread-safe

--- src/main/java/MHR/practice/singleton/DbSingleton.java
+++ src/main/java/MHR/practice/singleton/DbSingleton.java
@@ -2,12 +2,19 @@ package MHR.practice.singleton;
 
 public class DbSingleton {
 
-    private static DbSingleton instance = new DbSingleton();
-    private DbSingleton(){
+    //    private static DbSingleton instance = new DbSingleton();
+
+    private static class LazyHolder {
+        static final DbSingleton INSTANCE = new DbSingleton();
+    }
+
+    private DbSingleton() {
 
     }
 
-    public static DbSingleton getInstance(){
-        return instance;
+    public static DbSingleton getInstance() {
+
+        //    return instance;
+        return LazyHolder.INSTANCE;
     }
 }
